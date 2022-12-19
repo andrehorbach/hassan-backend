171e6d67c4e2ad4212a306ef1991620190812933
André Horbach <andrecomexdev@gmail.com>
2022-12-18 22:28:51 -0300

    Change to onRender server

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const mongoose = require("mongoose");
 const _ = require("lodash");
 const cors = require("cors");
 const db = require("./models/columns");
-//const { clients } = require("./app/models");
+
 
 const app = express();
 
 var corsOptions = {
-  origin: "https://hassan-frontend.herokuapp.com"
- // origin: "http://localhost:3000"
+  origin: "https://hassan-frontend.onrender.com"
+  //origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
@@ -20,26 +20,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(bodyParser.json());
-//mongoose.connect("mongodb+srv://admin-andre:flapinho@cluster0.acnw3.mongodb.net/hassan-board?retryWrites=true&w=majority", {useNewUrlParser: true});
+
 mongoose.connect("mongodb+srv://admin-andre:flapinho@cluster0.acnw3.mongodb.net/hassan-board?retryWrites=true&w=majority", {useNewUrlParser: true});
 
-
-// const clientsSchema = {
-//   id: String,
-//   content: Number,
-//   name: String,
-//   startDate: Date,
-//   injuries: Array,
-//   shock:  Number
-// }
-
-// const Client = mongoose.model("Client", clientsSchema);
 
 const columnsList = db.Column();
 
 app.get("/", function(req, res) {
   columnsList.find({}, (err, foundItems) => {
-    res.send(foundItems) // retirar [] se nao funcionar
+    res.send(foundItems) 
     console.log(foundItems);
   })
 });
@@ -54,13 +43,9 @@ app.post("/posts", function(req, res) {
     doc.save();
   })
 
-  // columnsList.insertMany(data, function(error, docs) {})
-  
   res.send("OK")
 
 });
-
-// app.put("/", function(req, res){
 
   let port = process.env.PORT;
 
